38e2559234b9b86b392e1edea7baba3d7ea24100
mokuno3430 <75469750+mokuno3430@users.noreply.github.com>
2025-01-14 16:50:18 +0900

    Upload initial version

---
+++ sample_data/figure3A/runme.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+db=db.txt
+query=query.txt
+blastn=blastn_plasmids.txt
+highlight=highlights_core.txt
+
+prefix=figure3A
+../../blastn2dotplot -i1 $db -i2 $query \
+    --blastn $blastn --min_identity 95 --min_alignlen 200 --figure_size 5 5 \
+    --highlight_crossed $highlight \
+    --show_grid \
+    --left_edge 0.12 --right_edge 0.95 \
+    --out $prefix 1> log.${prefix}
