1cb1e793def03389b639015220756ab2cd77af07
mokuno3430 <75469750+mokuno3430@users.noreply.github.com>
2025-05-13 10:00:13 +0900

    Update runme.sh

--- sample_data/figure3B/runme.sh
+++ sample_data/figure3B/runme.sh
@@ -6,7 +6,7 @@ blastn=blastn_mtDNA-vs-numt.txt
 grids=grids.txt
 
 prefix=figure3B
-../../blastn2dotplot -i1 $db -i2 $query \
+../../blastn2dotplots -i1 $db -i2 $query \
     --blastn $blastn --min_identity 98 --min_alignlen 200 --figure_size 4 5 \
     --manual_grid $grids --tick_width 50000 \
     --grid_color '#00bfff' \
